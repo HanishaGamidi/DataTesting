cf32685fd7e99dfbd4d32bab56c1558396daf64e
hadoop <hadoop_user@hadoop.us-central1-c.c.boreal-logic-368518.internal>
2022-12-20 05:04:15 +0000

    from vm

--- src/main/java/my/com/ReadWriteHDFSExample.java
+++ src/main/java/my/com/ReadWriteHDFSExample.java
@@ -6,28 +6,91 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-
+/*import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+*/
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class ReadWriteHDFSExample {
+private static String driverName = "org.apache.hive.jdbc.HiveDriver";
 
     public static void main(String[] args) throws IOException {
 //        ReadWriteHDFSExample.checkExists();
 //        ReadWriteHDFSExample.createDirectory();
 //        ReadWriteHDFSExample.checkExists();
-//        ReadWriteHDFSExample.writeFileToHDFS();
+          ReadWriteHDFSExample.writeFileToHDFS();
 //        ReadWriteHDFSExample.appendToHDFSFile();
-        ReadWriteHDFSExample.readFileFromHDFS();
+       // ReadWriteHDFSExample.readFileFromHDFS();
+	//ReadWriteHDFSExample.fetchHive();	
     }
 
+  /*  public static void fetchHive() throws SQLException {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(1);
+        }
+        //replace "hive" here with the name of the user the queries should run as
+        Connection con = DriverManager.getConnection("jdbc:hive2://10.10.10.10:10000", "hiveuser", "hivepassword");
+        //replace 10.10.10.10 with the Hive server address
+        Statement stmt = con.createStatement();
+        String tableName = "names";
+        stmt.execute("drop table if exists " + tableName);
+
+        stmt.execute("CREATE TABLE "+tableName+" (key int,value STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ','");
+
+        //stmt.execute("CREATE TABLE " + tableName + " (key int, value string)");
+        // show tables
+        String sql = "show tables '" + tableName + "'";
+        System.out.println("Running: " + sql);
+        ResultSet res = stmt.executeQuery(sql);
+        if (res.next()) {
+            System.out.println(res.getString(1));
+        }
+        // describe table
+        sql = "describe " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1) + "\t" + res.getString(2));
+        }
+
+        // load data into table
+        // NOTE: filepath has to be local to the hive server
+        String filepath = "/names.txt";
+        sql = "load data local inpath '" + filepath + "' into table " + tableName;
+        System.out.println("Running: " + sql);
+        stmt.execute(sql);
+
+        // select * query
+        sql = "select * from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2));
+        }
+
+        // regular hive query
+        sql = "select count(1) from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1));
+        }
+    }*/
     public static void readFileFromHDFS() throws IOException {
         Configuration configuration = new Configuration();
-        configuration.set("fs.defaultFS", "hdfs://localhost:9000");
+        configuration.set("fs.defaultFS", "hdfs://0.0.0.0:54310");
         FileSystem fileSystem = FileSystem.get(configuration);
         //Create a path
-        String fileName = "read_write_hdfs_example.txt";
-        Path hdfsReadPath = new Path("/user/javadeveloperzone/javareadwriteexample/" + fileName);
+        String fileName = "test.txt";
+        Path hdfsReadPath = new Path("/" + fileName);
         //Init input stream
         FSDataInputStream inputStream = fileSystem.open(hdfsReadPath);
         //Classical input stream usage
@@ -48,19 +111,21 @@ public class ReadWriteHDFSExample {
 
     public static void writeFileToHDFS() throws IOException {
         Configuration configuration = new Configuration();
-        configuration.set("fs.defaultFS", "hdfs://localhost:9000");
+        configuration.set("fs.defaultFS", "hdfs://0.0.0.0:54310");
         FileSystem fileSystem = FileSystem.get(configuration);
         //Create a path
-        String fileName = "read_write_hdfs_example.txt";
-        Path hdfsWritePath = new Path("/user/javadeveloperzone/javareadwriteexample/" + fileName);
+        String fileName = "test2.txt";
+        Path hdfsWritePath = new Path("/" + fileName);
         FSDataOutputStream fsDataOutputStream = fileSystem.create(hdfsWritePath,true);
 
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fsDataOutputStream,StandardCharsets.UTF_8));
-        bufferedWriter.write("Java API to write data in HDFS");
+        bufferedWriter.write("Welcome to Hadoop Automation !!");
         bufferedWriter.newLine();
         bufferedWriter.close();
         fileSystem.close();
     }
+
+
 
     public static void appendToHDFSFile() throws IOException {
         Configuration configuration = new Configuration();
